cf02031e0dff6fb2de881070b303c08e558e575f
Mizraelko <vkoporov@gmail.com>
2021-04-09 16:05:33 +0300

    add menu cool

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-
+"use strict"
 
 jQuery(function ($) {
 
-    'use strict';
+    
 
 
     /*================================================================*\
@@ -83,8 +83,94 @@ document.querySelector('.tablist').addEventListener('click', (e) => {
  
   
 })
-}
+};
+
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  IOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.IOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  }
+};
+
+if(isMobile.any()) {
+  document.body.classList.add('_touch');
+
+  let menuArrows = document.querySelectorAll('.menu__arrow-container');
+  if(menuArrows.length > 0) {
+    for (let index = 0; index < menuArrows.length; index++) {
+      const menuArrow = menuArrows[index];
+      
+      menuArrow.addEventListener('click', (e) => {
+        menuArrow.parentElement.classList.toggle('_active');
+        console.log(e.target)
+      })
+    }
+  }
+
+}else {
+  document.body.classList.add('_pc');
+};
+
+            //Бургер
+
+            
+
+  const menuIconContainer = document.querySelector('.menu__icon-container');
+  const menuBody = document.querySelector('.menu__body')
+  const iconMenu = document.querySelector('.menu__icon')
+ if(iconMenu) {
+  menuIconContainer.addEventListener('click', (e) => {
+      document.body.classList.toggle('_lock')
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   })
+ }           
+
+            //Прокрутка
   
+const menuLink = document.querySelector('.menu__link[data-goto]');
+
+menuLink.addEventListener('click', (e) => {
+ 
+  if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+      
+      if(iconMenu.classList.contains('_active')) {
+        document.body.classList.remove('_lock')
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+      }
+      
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      })
+      e.preventDefault();
+  }
+});
+
+
 
 
 
