4c2ebb1504c4a79e0ba55290f02667fc80b82434
Mizraelko <vkoporov@gmail.com>
2021-04-28 16:33:45 +0300

    add recaptcha

--- contacts.php
+++ contacts.php
@@ -1,6 +1,29 @@
 <?php
+
+$captcha;
+if(isset($_POST['token'])){
+	$captcha=$_POST['token'];
+}
+
+$secretKey = "6LeXL70aAAAAAJxSaswCF6itNIhufG5TAPPyJL7o";
+$ip = $_SERVER['REMOTE_ADDR'];
+
+// post request to server
+
+$url =  'https://www.google.com/recaptcha/api/siteverify?secret=' . $secretKey .  '&response=' . $_POST['token'];
+$response = file_get_contents($url);
+$responseKeys = json_decode($response,true);
+header('Content-type: application/json');
+if($responseKeys["success"] && $responseKeys["score"] >= 0.5) {
+	echo json_encode(array('success' => 'true', 'om_score' => $responseKeys["score"], 'token' => $_POST['token']));
+} else {
+	echo json_encode(array('success' => 'false', 'om_score' => $responseKeys["score"], 'token' => $_POST['token']));
+}
+
 if (isset ($_POST['email'])) {
+ 
   
+ 
   $to = "hudprokat@gmail.com"; // поменять на свой электронный адрес
   $from = $_POST['email'];
   $tel = $_POST['to_tel'];
@@ -48,5 +71,6 @@ $attachment";
   }
   
 }
+
 
 ?>
